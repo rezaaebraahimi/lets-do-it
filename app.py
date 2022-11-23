3f29d81a59d3cc468adb343ed3f27255ba80720c
rezaaebraahimi <rezaebrahimiproducton@gmail.com>
2022-11-23 20:22:07 +0330

    add some function and style

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
-from flask import Flask, render_template
+import datetime
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
+habits = ["Coding","Gaming"]
+
+def date_range(start: datetime.date):
+    dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
+    return dates
 
 @app.route("/")
-def todo():
-    return render_template("home.html", title="Lets Do IT - Home")
+def index():
+    date_str = request.args.get("date")
+    if date_str:
+        selected_date = datetime.date.fromisoformat(date_str)
+    else:
+        selected_date = datetime.date.today()
+        
+    return render_template("home.html",
+                           habits=habits,
+                           title="Let's Do IT - Home",
+                           date_range=date_range,
+                           selected_date=selected_date)
 
 
 
 @app.route("/add",  methods=["GET", "POST"])
 def add_habit():
-    return render_template("todo.html", title="Lets Do IT - Add Habit")
+    if request.method == "POST":
+        habits.append(request.form.get("habit"))
+    return render_template("todo.html", title="Let's Do IT - Add Habit")
 
