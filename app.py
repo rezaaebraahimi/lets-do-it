f95d385afba640faf7cff93c4fcf746168546257
rezaaebraahimi <rezaebrahimiproducton@gmail.com>
2022-11-24 01:40:29 +0330

    Upgrade-1.0.2

--- app.py
+++ app.py
@@ -1,51 +1,17 @@
-import datetime
-from collections import defaultdict
-from flask import Flask, render_template, request, redirect, url_for
+import os
+from flask import Flask
+from routes import pages
+from pymongo import MongoClient
+from dotenv import load_dotenv
 
 
-app = Flask(__name__)
-habits = ["Coding","Gaming"]
-completions = defaultdict(list)
-
-
-@app.context_processor
-def add_calc_date_range():
-    def date_range(start: datetime.date):
-        dates = [start + datetime.timedelta(days=diff) for diff in range(-3,4)]
-        return dates
-    
-    return {"date_range": date_range}
-
-
-@app.route("/")
-def index():
-    date_str = request.args.get("date")
-    if date_str:
-        selected_date = datetime.date.fromisoformat(date_str)
-    else:
-        selected_date = datetime.date.today()
-        
-    return render_template("home.html",
-                           habits=habits,
-                           title="Let's Do IT - Home",
-                           completions=completions[selected_date],
-                           selected_date=selected_date)
+load_dotenv()
 
 
 
-@app.route("/add",  methods=["GET", "POST"])
-def add_habit():
-    if request.method == "POST":
-        habits.append(request.form.get("habit"))
-    return render_template("todo.html",
-                           title="Let's Do IT - Add Habit",
-                           selected_date=datetime.date.today())
-
-
-@app.route("/complete", methods=["POST"])
-def complete():
-    date_string = request.form.get("date")
-    habit = request.form.get("habitName")
-    date = datetime.date.fromisoformat(date_string)
-    completions[date].append(habit)
-    return redirect(url_for("index",date=date_string))
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient(os.environ.get("MONGODB_URI"))
+    db = client.Todo
+    app.register_blueprint(pages)
+    return app
