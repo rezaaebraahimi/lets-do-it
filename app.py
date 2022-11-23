e0f4a052b28e3ee85a54d72caf3a7547b088bf35
rezaaebraahimi <rezaebrahimiproducton@gmail.com>
2022-11-23 20:59:07 +0330

    add func and style

--- app.py
+++ app.py
@@ -5,9 +5,16 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 habits = ["Coding","Gaming"]
 
-def date_range(start: datetime.date):
-    dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
-    return dates
+
+
+@app.context_processor
+def add_calc_date_range():
+    def date_range(start: datetime.date):
+        dates = [start + datetime.timedelta(days=diff) for diff in range(-3,4)]
+        return dates
+    
+    return {"date_range": date_range}
+
 
 @app.route("/")
 def index():
@@ -20,7 +27,6 @@ def index():
     return render_template("home.html",
                            habits=habits,
                            title="Let's Do IT - Home",
-                           date_range=date_range,
                            selected_date=selected_date)
 
 
@@ -29,5 +35,7 @@ def index():
 def add_habit():
     if request.method == "POST":
         habits.append(request.form.get("habit"))
-    return render_template("todo.html", title="Let's Do IT - Add Habit")
+    return render_template("todo.html",
+                           title="Let's Do IT - Add Habit",
+                           selected_date=datetime.date.today())
 
