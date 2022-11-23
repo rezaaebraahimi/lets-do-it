c0a4b22da500f7dcc41495e41645e470c070c560
rezaaebraahimi <rezaebrahimiproducton@gmail.com>
2022-11-23 19:02:56 +0330

    update files

--- app.py
+++ app.py
@@ -3,25 +3,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-todos = [
-    ("Get milk", False),
-    ("Learn programming", True)
-]
-
-
-
 @app.route("/")
 def todo():
-    return render_template("home.html", todos=todos)
+    return render_template("home.html", title="Lets Do IT - Home")
 
 
 
-@app.route("/<string:todo>")
-def todo_item(todo: str):
-    for text, completed in todos:
-        if text == todo:
-            completed_text = "[x]" if completed else "[]"
-            title = f"{completed_text} - Todos"
-            return render_template("todo.html", text=text, completed=completed, title=title)
-    else:
-        return render_template("not-found.html", text=todo, title="Not Found")
+@app.route("/add",  methods=["GET", "POST"])
+def add_habit():
+    return render_template("todo.html", title="Lets Do IT - Add Habit")
+
