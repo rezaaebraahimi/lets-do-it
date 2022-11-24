f39e029ee32ff99e6d495e25a81b63fcdbfdbf23
rezaaebraahimi <rezaebrahimiproducton@gmail.com>
2022-11-24 03:55:05 +0330

    fix and edit

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import os
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import datetime
 import uuid
-from flask import Flask, render_template, request, redirect, url_for
+
 
 load_dotenv()
 app = Flask(__name__)
 client = MongoClient(os.environ.get("MONGODB_URI"))
 db = client.Todo
+
 
 @app.context_processor
 def add_calc_date_range():
@@ -19,6 +21,10 @@ def add_calc_date_range():
     return {"date_range": date_range}
 
 
+def today_at_midnight():
+    today = datetime.datetime.today()
+    return datetime.datetime(today.year, today.month, today.day)
+
 
 @app.route("/")
 def index():
@@ -26,9 +32,9 @@ def index():
     if date_str:
         selected_date = datetime.datetime.fromisoformat(date_str)
     else:
-        selected_date = datetime.datetime.today()
+        selected_date = today_at_midnight()
         
-    habits_on_date = db.Todo.find({"added": {"$lte":selected_date}})    
+    habits_on_date = db.Todo.find({"added": selected_date})    
     Completed = [habit["habit"] for habit in db.Completed.find({"date":selected_date})]    
     return render_template("home.html",
                            habits=habits_on_date,
@@ -37,27 +43,24 @@ def index():
                            selected_date=selected_date)
 
 
-
-@app.route("/add",  methods=["POST", "GET"])
-def add_habit():
-
-    
-    if request.method == "POST":
-       db.Todo.insert_one(
-           {"_id":uuid.uuid4().hex, "added": datetime.datetime.today(), "name": request.form.get("habit")}
-       )
-    return render_template("todo.html",
-                           title="Let's Do IT - Add Habit",
-                           selected_date=datetime.datetime.today())
-
-
 @app.route("/complete", methods=["POST"])
 def complete():
     date_string = request.form.get("date")
-    habit = request.form.get("habitId")
     date = datetime.datetime.fromisoformat(date_string)
+    habit = request.form.get("habitId")
     db.Completed.insert_one({"date": date, "habit":habit})
     return redirect(url_for("index",date=date_string))
+
+
+@app.route("/add",  methods=["POST", "GET"])
+def add_habit():
+    today = today_at_midnight()
+    if request.method == "POST":
+        db.Todo.insert_one({"_id":uuid.uuid4().hex, "added": today, "name": request.form.get("habit")})
+    return render_template("todo.html",
+                           title="Let's Do IT - Add Habit",
+                           selected_date=today)
+
 
 
 if __name__ == "__main__":
