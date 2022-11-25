c969fbff08af0ccf0b732f7ceb1e2d9239f1c355
rezaaebraahimi <rezaebrahimiproducton@gmail.com>
2022-11-25 06:01:56 +0330

    fix issues

--- app.py
+++ app.py
@@ -29,28 +29,30 @@ def today_at_midnight():
 
 
 
-def select_date():
+def no_today():
     date_str = request.args.get("date")
     if date_str:
-        selected_date = datetime.datetime.fromisoformat(date_str)
-    else:
-        selected_date = today_at_midnight()
+        not_today = datetime.datetime.fromisoformat(date_str)
+        if datetime.datetime.today() != not_today:
+            selected_date = not_today
     
     return datetime.datetime(selected_date.year, selected_date.month, selected_date.day)
 
 
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
     date_str = request.args.get("date")
     if date_str:
-        selected_date = datetime.datetime.fromisoformat(date_str)
-        habits_on_date = db.Todo.find({"date": selected_date})
+        not_today = datetime.datetime.fromisoformat(date_str)
+        if datetime.datetime.today() != not_today:
+            selected_date = no_today()
+            habits_on_date = db.Todo.find({"date": selected_date})
+            Completed = [habit["name"] for habit in db.Completed.find({"date_complete":selected_date})]
     else:
         selected_date = today_at_midnight()
         habits_on_date = db.Todo.find({"date": selected_date})
-      
-    Completed = [habit["name"] for habit in db.Completed.find({"date_complete":selected_date})]    
+        Completed = [habit["name"] for habit in db.Completed.find({"date_complete":selected_date})]    
     
     return render_template("home.html",
                            habits=habits_on_date,
@@ -60,36 +62,38 @@ def index():
 
 
 
-@app.route("/complete", methods=["POST"])
+@app.route("/complete", methods=["POST", "GET"])
 def complete():
     date_string = request.form.get("date")
     date = datetime.datetime.fromisoformat(date_string)
     habit = request.form.get("habitName")
     db.Completed.insert_one({"date_complete": date, "name":habit})
+    db.Todo.delete_one({"date": date, "name":habit})
     return redirect(url_for("index",date=date_string))
 
 
 
 @app.route("/add",  methods=["POST", "GET"])
 def add_habit():
-
-        if datetime.datetime.today() != today_at_midnight():
-            selected_date = select_date()
+        date_str = request.args.get("date")
+        if date_str:
+            not_today = datetime.datetime.fromisoformat(date_str)
+            if datetime.datetime.today() != not_today:
+                selected_date = no_today()
             
-            if request.method == "POST":
-                db.Todo.insert_one({"_id":uuid.uuid4().hex,
+                if request.method == "POST":
+                    db.Todo.insert_one({"_id":uuid.uuid4().hex,
                                     "date": selected_date,
                                     "name": request.form.get("habit")})
         else:
-            selected_date = today_at_midnight()
-            if request.method == "POST":
-                db.Todo.insert_one({"_id":uuid.uuid4().hex,
+            today = datetime.datetime.today()
+            if datetime.datetime.today() == today:
+                selected_date = today_at_midnight()
+                if request.method == "POST":
+                    db.Todo.insert_one({"_id":uuid.uuid4().hex,
                                     "date": selected_date,
                                     "name": request.form.get("habit")})
-        
-        return render_template("todo.html",
-                           title="Let's Do IT - Add Habit",
-                           selected_date=selected_date)
+        return render_template("todo.html",title="Let's Do IT - Add Habit",selected_date=selected_date)
 
 
 
@@ -101,14 +105,11 @@ def show():
     else:
         selected_date = today_at_midnight()
         
-    habits_does = db.Todo.find({"date": {"$lte":selected_date}}) 
+    habits_does = db.Completed.find({"date_complete": {"$lte":selected_date}}) 
     allcompleted = [habit["name"] for habit in db.Completed.find({"date_complete":{"$lte":selected_date}})]
     
     return render_template("show.html",
-                           title="Let's Do IT - Show All",
-                           completions=allcompleted,
-                           habits=habits_does,
-                           selected_date=selected_date)
+                           title="Let's Do IT - Show All",allcompleted=allcompleted,habits_does=habits_does,selected_date=selected_date)
 
 
 
