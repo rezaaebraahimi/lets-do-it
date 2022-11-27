5a11affa90a53937bf40ffeeb3a4f2aeb0617fd4
rezaaebraahimi <rezaebrahimiproducton@gmail.com>
2022-11-27 20:23:12 +0330

    optimize and add comments

--- app.py
+++ app.py
@@ -6,30 +6,35 @@ import datetime
 import uuid
 
 
+
 load_dotenv()
 app = Flask(__name__)
-app.secret_key = "123reza"
+app.secret_key = "letsdoit"
 client = MongoClient(os.environ.get("MONGODB_URI"))
 db = client.Todo
-users = {}
 
 
-# Make a date range list for datetime bar 
+
+    # Make a range list of dates
+
 @app.context_processor
 def add_calc_date_range():
     def date_range(start: datetime.datetime):
         dates = [start + datetime.timedelta(days=diff) for diff in range(-3,4)]
         return dates
-    
     return {"date_range": date_range}
 
 
-# Today time format
+
+    # define today format time 
+
 def today_():
     today = datetime.datetime.today()
     return datetime.datetime(today.year, today.month, today.day)
 
 
+
+    # Main page function
 
 @app.route("/", methods=["POST", "GET"])
 def index():
@@ -39,11 +44,15 @@ def index():
         if datetime.datetime.today() != not_today:
             selected_date = not_today
             habits_on_date = db.Todo.find({"date": selected_date})
-            Completed = [habit["name"] for habit in db.Completed.find({"date_complete":selected_date})]
+            Completed = [
+                habit["name"] for habit in db.Completed.find(
+                    {"date_complete":selected_date})]
     else:
         selected_date = today_()
         habits_on_date = db.Todo.find({"date": selected_date})
-        Completed = [habit["name"] for habit in db.Completed.find({"date_complete":selected_date})]    
+        Completed = [
+            habit["name"] for habit in db.Completed.find(
+                {"date_complete":selected_date})]    
     
     return render_template("home.html",
                            habits=habits_on_date,
@@ -52,6 +61,8 @@ def index():
                            selected_date=selected_date)
 
 
+
+    # when a task complete 
 
 @app.route("/complete", methods=["POST", "GET"])
 def complete():
@@ -64,6 +75,7 @@ def complete():
     return redirect(url_for("index",date=date_string))
 
 
+    # add new daily task
 
 @app.route("/add",  methods=["POST", "GET"])
 def add_habit():
@@ -74,9 +86,13 @@ def add_habit():
                                     "name": request.form.get("habit")})
         flash("New task successfully added!", "info")
         return redirect(url_for('index'))
-    return render_template("todo.html",title="Let's Do IT - Add Habit",selected_date=today)
+    return render_template("todo.html",
+                           title="Let's Do IT - Add Habit",
+                           selected_date=today)
 
 
+
+    # show all of the completed tasks
 
 @app.route("/show", methods = ["POST", "GET"])
 def show():
@@ -87,30 +103,25 @@ def show():
         selected_date = today_()
         
     habits_does = db.Completed.find({"date_complete": {"$lte":selected_date}}) 
-    allcompleted = [habit["name"] for habit in db.Completed.find({"date_complete":{"$lte":selected_date}})]
+    allcompleted = [
+        habit["name"] for habit in db.Completed.find(
+            {"date_complete":{"$lte":selected_date}})]
     
     return render_template("show.html",
-                           title="Let's Do IT - Show All",allcompleted=allcompleted,habits_does=habits_does,selected_date=selected_date)
+                           title="Let's Do IT - Show All",
+                           allcompleted=allcompleted,
+                           habits_does=habits_does,
+                           selected_date=selected_date)
 
 
+
+    # delete all of the completed tasks
 
 @app.route('/delete_completed', methods = ["POST", "GET"])
 def delete_completed():
     db.Completed.delete_many({})
     flash("Completed tasks has been deleted!", "error")
     return redirect(url_for('index'))
-
-
-
-app.route("/signup", methods = ["POST","GET"])
-def signup():
-    if request.method == "POST":
-        email = request.form.get("email")
-        password = request.form.get("password")
-        
-        users[email] = password
-        flash("Successfully signed up.")
-        
 
 
 if __name__ == "__main__":
